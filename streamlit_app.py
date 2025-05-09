35f170091922760917fcfffb04566363faee057c
yz2153 <coffeeyz2368@gmail.com>
2025-05-09 13:48:42 +0000

    0509_2148

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-
+ 
+# 設定頁面的標題與副標題(模式選擇)
 st.title(":cup_with_straw: The Destined Pour")
 st.header("Select the generator mode you want!")
 
+# 重置三種限制的st.session_state
 if 'calories_customized' not in st.session_state:
     st.session_state['calories_customized'] = 'NO'
 if 'price_customized' not in st.session_state:
@@ -10,11 +12,15 @@ if 'price_customized' not in st.session_state:
 if 'ingredient_customized' not in st.session_state:
     st.session_state['ingredient_customized'] = 'NO'
 
+# 重置用來放模式選擇結果的list
+mode_badge_list = []
 
+# def用來處理calories功能的開關的function
 def calories_on_change():
     st.session_state['calories_customized'] = st.session_state["calories_temp"]
+    return None
 
-# st.markdown("<p style='font-size:16px; color:Black; font-weight:bold;'>1. Do you want to customize the “calories” of your drinks?</p>", unsafe_allow_html=True)
+# 設定calories mode的選擇區
 option_calories = st.radio(
     ":one: Do you want to customize the “calories” of your drinks?",
     [":rainbow[YES]", "NO",],
@@ -25,10 +31,12 @@ option_calories = st.radio(
 )
 # if option_calories == ':rainbow[YES]':
 
-
+# def用來處理price功能的開關的function
 def price_on_change():
     st.session_state['price_customized'] = st.session_state["price_temp"]
+    return None
 
+# 設定price mode的選擇區
 option_price = st.radio(
     ":two: Do you want to customize the “price” of your drinks?",
     [":rainbow[YES]", "NO",], 
@@ -38,14 +46,41 @@ option_price = st.radio(
     horizontal=True,
 )
 
+# def用來處理ingredient功能的開關的function
+def ingredient_on_change():
+    st.session_state['ingredient_customized'] = st.session_state["ingredient_temp"]
+    return None
 
-
+# 設定ingredient mode的選擇區
 option_ingredient = st.radio(
     ":three: Do you want to customize the “ingredient” of your drinks?",
-    [":rainbow[YES]", "NO",],    
+    [":rainbow[YES]", "NO",],  
+    key="ingredient_temp",  
     index=1,
+    on_change=ingredient_on_change,
     horizontal=True,
 )
+
+
+if st.session_state['calories_customized'] != 'NO':
+    badge_calories = ':orange-badge[Calories]'
+    # mode_badge_list.append(st.badge("Calories", color = 'orange'))
+else:
+        badge_calories = ''
+        # mode_badge_list.remove(st.badge("Calories", color = 'green'))
+
+if st.session_state['price_customized'] != 'NO':
+    badge_price = ':green-badge[Price]'
+    # mode_badge_list.append(st.badge("Price", color = 'green'))
+else:
+        badge_price = ''
+        # mode_badge_list.remove(st.badge("Price", color = 'green'))
+
+if st.session_state['ingredient_customized'] != 'NO':
+    badge_ingredient = ':blue-badge[Ingredient]'
+else:
+        badge_ingredient = ''
+        # mode_badge_list.remove(st.badge("Price", color = 'green'))
 
 
 if option_calories == 'NO' and option_price == 'NO' and option_ingredient == 'NO':
@@ -54,11 +89,15 @@ if option_calories == 'NO' and option_price == 'NO' and option_ingredient == 'NO
     ✔️ You selected: Random generator </div>""",
     unsafe_allow_html=True)
 
-else: # 還沒改好 要可以適應上面的選項
-    st.markdown(f"""
-    <div style='font-size:18px; font-weight:bold;'>
-    ✔️ You selected: ? </div>""",
-    unsafe_allow_html=True)
+else: 
+    col1, col2 = st.columns([1, 6])  # 調整比例讓 badge 不太擠
+    with col1:
+        st.markdown("這是一個超人氣飲料")
+    
+    with col2:
+        st.markdown(
+            badge_calories + badge_price + badge_ingredient
+        )
 
 
 st.divider()
@@ -69,7 +108,12 @@ if option_calories == ':rainbow[YES]':
     st.write("You're scheduled for:", budget)
 
 
-#下方皆為舊版code
+
+
+
+#---下方皆為舊版code---
+#---下方皆為舊版code---
+#---下方皆為舊版code---
 st.markdown("<p style='font-size:20px; color:DarkMagenta; font-weight:bold;'>Which mode would you like to try?</p>", unsafe_allow_html=True)
 option = st.selectbox(
     "",
